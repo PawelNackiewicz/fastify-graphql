f19bc7695951015614a576e8c3e2f22b163d384a
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2022-08-24 23:01:15 +0200

    add prisma

---
+++ prisma/migrations/20220824210007_init/migration.sql
@@ -0,0 +1,113 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
+
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "VisitStatus" AS ENUM ('DONE', 'CANCELLED', 'BEFORE_APPLICATION');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "login" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "expireAt" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL DEFAULT 'FEMALE',
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "email" TEXT,
+    "phone" TEXT,
+    "points" INTEGER NOT NULL DEFAULT 0,
+    "source" TEXT,
+    "photoUrl" TEXT,
+    "instagram" TEXT,
+    "facebook" TEXT,
+    "notes" TEXT,
+    "marketingPermission" BOOLEAN NOT NULL DEFAULT true,
+    "healthContraindications" TEXT,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "duration" TIMESTAMP(3),
+    "price" INTEGER NOT NULL,
+    "priority" "Priority" NOT NULL DEFAULT 'MEDIUM',
+
+    CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Visit" (
+    "id" SERIAL NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL,
+    "status" "VisitStatus" NOT NULL DEFAULT 'BEFORE_APPLICATION',
+    "notes" TEXT,
+    "customerId" INTEGER NOT NULL,
+    "serviceId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Visit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_token_key" ON "Token"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Visit_customerId_key" ON "Visit"("customerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Visit_serviceId_key" ON "Visit"("serviceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Visit_userId_key" ON "Visit"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Visit" ADD CONSTRAINT "Visit_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Visit" ADD CONSTRAINT "Visit_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Visit" ADD CONSTRAINT "ScheduledVisit_userId" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Visit" ADD CONSTRAINT "Visit_userId" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
